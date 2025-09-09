65d3c3f01512481927a34ae3a106f2fb0494d70d
AliG023 <ali.g023.bc@gmail.com>
2025-09-09 15:30:38 -0400

    Host address update

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   preview: {
     allowedHosts: [
-      'https://portfolio-0h77.onrender.com'
+      'https://ali-graham-portfolio.onrender.com'
     ]
   }
 })
