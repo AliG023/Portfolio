85c36551bfea7564f388fda74188c485847504d5
AliG023 <ali.g023.bc@gmail.com>
2025-09-09 21:01:34 -0400

    Nav Update

---
+++ src/components/Education.jsx
@@ -0,0 +1,5 @@
+export default function Education () {
+    <>
+        <div>Education</div>
+    </>
+}
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -6,7 +6,10 @@ export default function Layout() {
             <h1>My Portfolio</h1>
             <nav>
                 <Link to="/">Home</Link>  |  
-                <Link to="/about"> About</Link>  |  
+                <Link to="/about"> About</Link>  |
+                <Link to="/project"> Projects</Link>  |
+                <Link to="/education"> Education</Link>  | 
+                <Link to="/service"> Services</Link>  |   
                 <Link to="/contact"> Contact</Link> 
             </nav>
         </>
