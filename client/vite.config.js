25fc28994eb71da671443ba42d06659c7b754b5e
AliG023 <ali.g023.bc@gmail.com>
2025-11-10 19:41:07 -0500

    Update Vite config for auth proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,11 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/auth': 'http://localhost:3000'
-    }
-  },
   preview: {
     allowedHosts: [
       "ali-graham-portfolio.onrender.com"
