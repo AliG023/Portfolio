a99539dbf520dd77c27d92772b60161403fcf6b8
AliG023 <ali.g023.bc@gmail.com>
2025-11-15 16:34:06 -0500

    bug fix

--- server/server.js
+++ server/server.js
@@ -8,30 +8,18 @@ import UserRouter from './routes/user.route.js'
 import QualificationsRouter from './routes/qualification.route.js'
 import ContactsRouter from './routes/contacts.route.js'
 
-
 mongoose.Promise = global.Promise
-mongoose.connect(config.mongoUri, { 
-    // useNewUrlParser: true,
-    // useUnifiedTopology: true,
-    // useCreateIndex: true
- } )
+mongoose.connect(config.mongoUri, { } )
     .then(() => {
         console.log("Connected to MongoDB!");
     })
 
 mongoose.connection.on('error', () => {
-throw new Error(`unable to connect to MongoDB: ${config.mongoUri}`)
+    throw new Error(`unable to connect to MongoDB: ${config.mongoUri}`)
 });
 
 app.get("/", (req, res) => {
-res.json({ message: "Welcome to My Portfolio application." });
-});
-
-app.listen(config.port, (err) => { 
-if (err) {
-console.log(err) 
-}
-console.info('Server started on port %s.', config.port) 
+    res.json({ message: "Welcome to My Portfolio application." });
 });
 
 // ROUTES
@@ -40,4 +28,12 @@ app.use('/', ProjectRouter);
 app.use('/', UserRouter);  
 app.use('/', QualificationsRouter);
 app.use('/', ContactsRouter);
+
+
+app.listen(config.port, (err) => { 
+    if (err) {
+        console.log(err) 
+    }
+    console.info('Server started on port %s.', config.port)  // This should print the actual number
+});
 
